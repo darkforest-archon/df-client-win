2cd2215eb6bb620accfffce42f4ab39a5e05726a
zhaochunyou <snowtigersoft@126.com>
2021-11-27 19:53:07 +0800

    sponsor planets

--- src/Backend/Network/LeaderboardApi.ts
+++ src/Backend/Network/LeaderboardApi.ts
@@ -1,10 +1,10 @@
 import { Leaderboard } from '@darkforest_eth/types';
-import {getEthConnection, loadCoreContract} from "./Blockchain";
+import { getEthConnection, loadCoreContract } from "./Blockchain";
 import { CORE_CONTRACT_ADDRESS } from "@darkforest_eth/contracts";
-import {EthAddress} from "@darkforest_eth/types/dist/identifier";
+import { EthAddress } from "@darkforest_eth/types/dist/identifier";
+import { DAO_A_CONTRACT_ADDRESS, DAO_B_CONTRACT_ADDRESS } from "../Utils/constants";
+import { EMPTY_ADDRESS } from "@darkforest_eth/constants";
 
-const DAO_A_CONTRACT_ADDRESS = "0xEcC8572e9ffCA02Aba6f89D127541b0984a6B511"
-const DAO_B_CONTRACT_ADDRESS = "0x27E0053f853dB33588D18030e79aCECd52C8bfC4"
 
 export async function loadLeaderboard(): Promise<Leaderboard> {
   return getEthConnection()
@@ -20,12 +20,18 @@ export async function loadLeaderboard(): Promise<Leaderboard> {
         return Promise.all([a, b])
       })
     .then((players) => {
-      console.log(players);
-      return {entries: players.map((p) => {return {
-        score: Number(p.score),
-        ethAddress: p.player as EthAddress,
-        twitter: p.player.toLowerCase() === DAO_A_CONTRACT_ADDRESS.toLowerCase() ? "Red Team" : "Blue Team"
-      }})};
+      return {
+        entries: players
+          .filter(p => p.player !== EMPTY_ADDRESS)
+          .map((p) => {
+              return {
+                score: Number(p.score),
+                ethAddress: p.player as EthAddress,
+                twitter: p.player.toLowerCase() === DAO_A_CONTRACT_ADDRESS.toLowerCase() ? "Red Team" : "Blue Team"
+              }
+            }
+          )
+      };
     })
       .catch((e) => {
         alert('error connecting to blockchain');
---
+++ src/Backend/Utils/constants.ts
@@ -0,0 +1,7 @@
+import { LocationId } from "@darkforest_eth/types";
+
+export const DAO_A_CONTRACT_ADDRESS = "0xEcC8572e9ffCA02Aba6f89D127541b0984a6B511";
+export const DAO_B_CONTRACT_ADDRESS = "0x27E0053f853dB33588D18030e79aCECd52C8bfC4";
+
+export const SPONSOR_PLANETS = new  Map<LocationId, string>();
+SPONSOR_PLANETS.set("3016d836df636814afcdb0ec4e0748e9c88469d4be078d78204273f92405b264" as LocationId, "Guild W Home");
